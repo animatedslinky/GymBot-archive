b6bc832bc5419e7a5a91099ac9fa221e7a50d72e
animatedslinky <slinky@digitalsojourn.com>
2017-07-24 18:42:40 -0400

    2 quick fixes. 1) missed adding files.  2) disable gym leader textbox when updating a gym to prevent it treating it as an add

---
+++ Slinkybot/AddUpdateDialog.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace Slinkybot
+{
+    /// <summary>
+    /// Interaction logic for AddUpdateDialog.xaml
+    /// </summary>
+    public partial class AddUpdateDialog : Window
+    {
+        public GymLeader leader;
+
+        
+        public AddUpdateDialog()
+        {
+            InitializeComponent();
+        }
+
+        public AddUpdateDialog(GymLeader lead)
+        {
+            InitializeComponent();
+            txtName.Text = lead.Name;
+            cbogymType.SelectedIndex = (lead.gymType == GymType.Elite4) ? 0 : 1;
+            txtGymUp.Text = lead.gymUpMessage;
+            txtGymDown.Text = lead.gymDownMessage;
+            txtName.IsEnabled = false;
+        }
+
+        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            leader = new GymLeader
+            {
+                Name = txtName.Text,
+                Online = "Offline",
+                gymType = cbogymType.SelectedValue.ToString().Equals("Elite 4") ? GymType.Elite4 : GymType.Gym,
+                offlineCountdown = 0,
+                gymUpMessage = txtGymUp.Text,
+                gymDownMessage = txtGymDown.Text
+            };
+            this.DialogResult = true;
+        }
+    }
+}
---
+++ Slinkybot/SelectLeaderDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Slinkybot
+{
+    /// <summary>
+    /// Interaction logic for SelectLeaderDialog.xaml
+    /// </summary>
+    public partial class SelectLeaderDialog : Window
+    {
+        public string SelectedLeader
+        {
+            get
+            {
+                return cboLeaders.SelectedValue.ToString();
+            }
+        }
+
+        public SelectLeaderDialog(ObservableCollection<GymLeader> gymLeaders,string title)
+        {
+            InitializeComponent();
+
+            this.Title = title;
+
+            foreach (GymLeader leader in gymLeaders)
+            {
+                cboLeaders.Items.Add(leader.Name);
+            }
+        }
+
+        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
